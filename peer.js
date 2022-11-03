33134a58c0f367b0e04f3fa8cd42e126c6dae0f1
Nice <naserdin@protonmail.com>
2022-11-03 21:44:30 +0800

    feat: just run peer

--- peer.js
+++ peer.js
@@ -1,4 +1,3 @@
 const { PeerServer } = require('peer');
-const PORT = process.env.PORT || 3001
-const peerServer = PeerServer({ port: PORT, path: '/peer' })
+const peerServer = PeerServer({ path: '/peer', port: 443 })
 console.log(`peer server run on port ${PORT}`)
